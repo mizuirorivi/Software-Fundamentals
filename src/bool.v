475bc5420c650f49be1a6000de94334a68bb1275
mizuirorivi <onodaha@gmail.com>
2022-05-31 15:06:02 +0900

    haskellにexportするのを確認

--- src/bool.v
+++ src/bool.v
@@ -1,6 +1,8 @@
 Inductive bool : Type :=
-| true : bool
-| false : bool.
+  | true : bool
+  | false : bool.
+
+
 
 Definition negb (b:bool) : bool :=
   match b with
@@ -8,29 +10,29 @@ Definition negb (b:bool) : bool :=
   | false => true
   end.
 
-Definition andb (b1: bool) (b2:bool) : bool :=
+Definition andb (b1:bool) (b2:bool) : bool :=
   match b1 with
   | true => b2
   | false => false
   end.
 
-Definition orb (b1: bool) (b2:bool) : bool :=
+Definition orb (b1:bool) (b2:bool) : bool :=
   match b1 with
   | true => true
   | false => b2
   end.
 
-(* orbのすべてのテストケース *)
 Example test_orb1: (orb true false) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_orb2: (orb false false) = false.
 Proof. simpl. reflexivity. Qed.
-Example test_orb3: (orb false true) = true.
+Example test_orb3: (orb false true ) = true.
 Proof. simpl. reflexivity. Qed.
-Example test_orb4: (orb true true) = true.
+Example test_orb4: (orb true true ) = true.
 Proof. simpl. reflexivity. Qed.
 
+Definition admit {T: Type} : T. Admitted.
+
 Require Extraction.
-(* Extraction Language: Haskell *)
 Extraction Language Haskell.
-Extraction "../extraction/bool.hs bool.
+Extraction "../extraction/bool.hs" bool negb orb andb.
