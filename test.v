461efc1f5b4106ff4b4dba1e252958af3f50d9ed
mizuirorivi <onodaha@gmail.com>
2022-05-31 14:55:49 +0900

    resolve conflict

---
+++ test.v
@@ -0,0 +1,29 @@
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+| sunday : day.
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+Eval simpl in (next_weekday friday).
+Eval simpl in (next_weekday (next_weekday saturday)).
+
+Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+
+Proof. simpl. reflexivity. Qed.
+
+
